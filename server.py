1bfcf952c0769fa34fe750eb94c0091865926338
Nick <kaplanicolas@gmail.com>
2025-05-09 01:15:22 -0400

    v3 - refactored website
    - added alpha 4 update info
    - refactored website

---
+++ server.py
@@ -0,0 +1,14 @@
+import http.server
+import socketserver
+
+PORT = 8000
+DIRECTORY = "."
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    print("Press Ctrl+C to stop the server")
+    httpd.serve_forever()
